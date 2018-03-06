6a2c978bb5c0f0c97cb694c154421343791cd65b
SalasDimasCarlos <carlos.salas@alumnos.udg.mx>
2018-03-06 08:16:11 -0600

    LET added to variables i j

--- calculadora.js
+++ calculadora.js
@@ -1,9 +1,12 @@
 var i;
 var j;
-for (i = 1 ; i < 13 ; i++ )
+
+//Let added to varible
+
+for (let i = 1 ; i < 13 ; i++ )
 {
   console.log("----- TABLA DEL "+ i + "---------------");
-  for(j = 1; j < 13; j++)
+  for(let j = 1; j < 13; j++)
   {
     console.log(i + " x " + j + " = " + i*j);
   }
