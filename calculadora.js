df50f2f24f85f3c72e5c3bdf12a9baa42296525f
SalasDimasCarlos <carlos.salas@alumnos.udg.mx>
2018-03-06 07:54:55 -0600

    Se le añadieron espacios a la tabla de 12x12=144 a 12 x 12 = 144

--- calculadora.js
+++ calculadora.js
@@ -2,9 +2,10 @@ var i;
 var j;
 for (i = 1 ; i < 13 ; i++ )
 {
+  console.log("----- TABLA DEL "+ i + "---------------");
   for(j = 1; j < 13; j++)
   {
-    console.log(i + "x" + j + "=" + i*j);
+    console.log(i + " x " + j + " = " + i*j);
   }
-  console.log("--------------------");
+
 }
