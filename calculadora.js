e5019bec915d85f46d8383473950e9796b2477a6
SalasDimasCarlos <carlos.salas@alumnos.udg.mx>
2018-03-06 07:50:43 -0600

    Practica calculadora terminada

--- calculadora.js
+++ calculadora.js
@@ -4,7 +4,7 @@ for (i = 1 ; i < 13 ; i++ )
 {
   for(j = 1; j < 13; j++)
   {
-    console.log(i + "x" + j +"=" + i*j);
+    console.log(i + "x" + j + "=" + i*j);
   }
-
+  console.log("--------------------");
 }
