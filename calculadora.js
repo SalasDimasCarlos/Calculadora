49912b04b8e33940082fac6a9725688f5f5d8ac6
SalasDimasCarlos <carlos.salas@alumnos.udg.mx>
2018-03-06 08:34:09 -0600

    IIFE added to Tablas function

--- calculadora.js
+++ calculadora.js
@@ -1,14 +1,19 @@
-var i;
-var j;
 
-//Let added to varible
 
-for (let i = 1 ; i < 13 ; i++ )
-{
-  console.log("----- TABLA DEL "+ i + "---------------");
-  for(let j = 1; j < 13; j++)
+(function () {
+  var i;
+  var j;
+
+  //Let added to varible
+
+  for (let i = 1 ; i < 13 ; i++ )
   {
-    console.log(i + " x " + j + " = " + i*j);
+    console.log("----- TABLA DEL "+ i + "---------------");
+    for(let j = 1; j < 13; j++)
+    {
+      console.log(i + " x " + j + " = " + i*j);
+    }
+
   }
 
-}
+})();
