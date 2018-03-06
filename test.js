9f77142249c12bdd86a10b361ab57c4bc155b65c
SalasDimasCarlos <carlos.salas@alumnos.udg.mx>
2018-03-06 08:13:44 -0600

    JS added to test global variables

---
+++ test.js
@@ -0,0 +1,13 @@
+switch (i) {
+  case 1:
+    console.log('A');
+    break;
+  case 2:
+    console.log('B');
+    break;
+  case 3:
+    console.log('C');
+    break;
+  default:
+    console.log("Tiene un valor diferente");
+}
